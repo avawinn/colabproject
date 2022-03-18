9a8072fd80fde55aabb62ef64d98d619c9c5f243
avawinn <101885991+avawinn@users.noreply.github.com>
2022-03-18 12:47:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,42 @@
 namespace SpriteKind {
     export const goal = SpriteKind.create()
 }
+let soccerball = darts.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 1 1 f f 1 1 1 . . . . . 
+    . . 1 1 1 1 1 1 1 1 1 1 1 . . . 
+    . . f f 1 1 1 1 1 1 f f 1 . . . 
+    . . f f 1 1 f f 1 1 f f 1 . . . 
+    . . 1 1 1 1 f f 1 1 1 1 1 . . . 
+    . . 1 1 1 1 1 1 1 1 1 1 1 . . . 
+    . . 1 1 1 1 1 1 1 1 1 1 1 . . . 
+    . . 1 f f 1 1 1 1 f f 1 1 . . . 
+    . . 1 f f 1 1 1 1 f f 1 1 . . . 
+    . . . 1 1 1 1 1 1 1 1 1 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+let football = darts.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . e e e e e e e e . . . . 
+    . . . e e e e e e e e e e . . . 
+    . . e e 1 e 1 e 1 e 1 e e e . . 
+    . e e 1 1 1 1 1 1 1 1 1 e e . . 
+    . e e e 1 e 1 e 1 e 1 e e e . . 
+    . . e e e e e e e e e e e . . . 
+    . . . e e e e e e e e e e . . . 
+    . . . . e e e e e e e e . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
 let gametype = game.askForString("Do you want to play soccer or football?", 8)
 scene.setBackgroundImage(img`
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
